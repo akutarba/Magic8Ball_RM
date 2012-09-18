a3b2f511fc86d344af05a2c165adc44266787b46
akutarba <anna.kutarba@jetbrains.com>
2012-09-18 14:35:28 +0400

    add model and resources

--- app/magic8_ball_view_controller.rb
+++ app/magic8_ball_view_controller.rb
@@ -3,6 +3,7 @@ class Magic8BallViewController < UIViewController
   def loadView
     self.view = UIImageView.alloc.init
   end
+
   def viewDidLoad
     #labelFrame = CGRectMake (10,60,300,80)
     view.image = UIImage.imageNamed('background.png')
@@ -12,15 +13,32 @@ class Magic8BallViewController < UIViewController
 
     view.addSubview(@label)
     view.userInteractionEnabled=true
-    recognizer = UITapGestureRecognizer.alloc.initWithTarget(self, action:'showAnswer')
+    recognizer = UITapGestureRecognizer.alloc.initWithTarget(self, :action => 'showAnswer')
     view.addGestureRecognizer(recognizer)
     @magicBall = Magic8ball.new
   end
+
   def showAnswer
-    @label.text = ['yes','no','maybe','try again'].sample
+    #@label.text = ['yes','no','maybe','try again'].sample
+    #@label.text = @magicBall.randomAnswer
+    #@label.alpha = 0
+    UIView.animateWithDuration(1.0,
+                               :animations => lambda {
+                                 @label.alpha=0
+                                 @label.transform=CGAffineTransformMakeScale(0.1, 0.1)
+                               },
+                               completion: lambda { |finished|
+                                 @label.text=@magicBall.randomAnswer
+                                 UIView.animateWithDuration(1.0,
+                                                            animations: lambda {
+                                                              @label.alpha=1
+                                                              @label.transform=CGAffineTransformIdentity
+                                                            })
+                               })
   end
+
   def makeLabel
-    label = UILabel.alloc.initWithFrame([[10,60],[300,80]])
+    label = UILabel.alloc.initWithFrame([[10, 60], [300, 80]])
     label.backgroundColor=UIColor.lightGrayColor
     label.text="Tap to Answer"
     label.font=UIFont.boldSystemFontOfSize(34)
--- app/magic_8ball.rb
+++ app/magic_8ball.rb
@@ -1,12 +1,38 @@
 class Magic8ball
   # To change this template use File | Settings | File Templates.
   def initialize
-    @answers = ["Yes","No","Maybe", "Try again"]
+    @answers = loadAnswers
   end
+
   def randomAnswer
     @answers.sample
   end
+
+  def loadAnswers
+    answerFile = NSBundle.mainBundle.pathForResource('answers', ofType: 'json')
+
+    errorPointer = Pointer.new(:object)
+
+    data = NSData.alloc.initWithContentsOfFile(answerFile,
+                                               options: NSDataReadingUncached,
+                                               error: errorPointer)
+    unless data
+      printError errorPointer[0]
+      return defaultAnswers
+    end
+    json = NSJSONSerialization.JSONObjectWithData(data,
+                                                  options: NSDataReadingUncached,
+                                                  error: errorPointer)
+    unless json
+      printError errorPointer[0]
+      return defaultAnswers
+    end
+    json['answers']
+
+  end
+
   def defaultAnswers
+    ['Yes', 'No', 'Maybe', 'Try Again']
 
 =begin
     name = "ruby motion"
@@ -15,6 +41,10 @@ class Magic8ball
     arr.insertObject("Motion", atIndex:2)
 =end
 
+  end
+
+  def printError(error)
+    $stderr.puts "Error: #{error.description}"
   end
 
 end
